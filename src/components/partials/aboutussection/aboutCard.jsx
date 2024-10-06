95c19921e3b76befc7f74090c3ae02402a4eb855
jund-fauz <mujadidjundifauzi200@gmail.com>
2024-10-06 19:40:59 +0700

    add: About Us Icon

--- src/components/partials/aboutussection/aboutCard.jsx
+++ src/components/partials/aboutussection/aboutCard.jsx
@@ -1,1 +1,12 @@
-export default function aboutCard() {}
+export default function AboutCard({image, number, isPlus = true, title}) {
+    return (
+			<div className='flex flex-col items-center' style={{ flex: 1 }}>
+				<img src={`/${image}_icon.svg`} alt={title} width='100vw' />
+				<h1 className='mt-6'>
+					{number}{' '}
+					<span style={{ color: '#ff3b30' }}>{isPlus ? '+' : '%'}</span>
+				</h1>
+				<h1 className='text-xl'>{title}</h1>
+			</div>
+		)
+}
